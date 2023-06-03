51f894c6ef40403ff934b7fd97108af384047f5c
hieuduong <hieuark2003@gmai.com>
2023-06-03 20:34:41 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://shoe-app-0ipl.onrender.com'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
 }
